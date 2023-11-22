ce0cf9512d50074267cb7829ea2dd001e2ee8b37
Sasorishi <on_alban@yahoo.fr>
2023-11-22 19:41:47 +0100

    Fix images background

---
+++ injuretice/GeneratorController.swift
@@ -0,0 +1,8 @@
+//
+//  GeneratorController.swift
+//  injuretice
+//
+//  Created by Alban On on 22/11/2023.
+//
+
+import Foundation
---
+++ injuretice/RandomUser.swift
@@ -0,0 +1,8 @@
+//
+//  RandomUser.swift
+//  injuretice
+//
+//  Created by Alban On on 22/11/2023.
+//
+
+import Foundation
